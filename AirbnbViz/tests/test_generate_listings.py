0b5b5347438f7b7c3f18ba088a5506dffbf53099
Beiqi Wang <becky@Beiqis-MBP.domain>
2017-06-07 14:46:15 -0700

    modify tests

--- AirbnbViz/tests/test_generate_listings.py
+++ AirbnbViz/tests/test_generate_listings.py
@@ -1,7 +1,9 @@
 import os
 import unittest
 import urllib3
+import filecmp
 from generate_listings import generate_listings
+
 
 file_path = "../html/js/listings.js"
 
@@ -11,6 +13,10 @@ class TestCase(unittest.TestCase):
         generate_listings()
         is_exist = os.path.exists(file_path)
         self.assertTrue(is_exist)
+
+    # case: if file is what we need used in html
+    def test_file_identity(self):
+		self.assertTrue(filecmp.cmp(file_path, 'files/listings.js'))
 
 
 if __name__ == '__main__':
